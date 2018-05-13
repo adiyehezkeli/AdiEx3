8f09718777b577d718a154fef9748d86e586eb27
Adi Yehezkeli <adi.yehezkeli@gmail.com>
2018-05-13 17:38:31 +0300

    bug fixed

--- app/src/main/java/com/adiyehezkeli/ex3threads/MainActivity.java
+++ app/src/main/java/com/adiyehezkeli/ex3threads/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        thisContext = this;
+        thisContext = this;
     }
 
     public void AsyncTaskActivity(View view)
